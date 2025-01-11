a509137b0a4712d10a5674029c4a877e7990644c
Tiago Teixeira Santos <tiagots86@hotmail.com>
2025-01-11 09:26:17 -0300

    estrutura inicial e iam_cluster

---
+++ environment/prod/terraform.tfvars
@@ -0,0 +1,23 @@
+project_name = "linuxtips-cluster"
+region       = "us-east-1"
+
+ssm_vpc = "/linuxtips-vpc/vpc/id"
+
+ssm_public_subnets = [
+  "/linuxtips-vpc/subnets/public/us-east-1a/linuxtips-public-1a",
+  "/linuxtips-vpc/subnets/public/us-east-1b/linuxtips-public-1b",
+  "/linuxtips-vpc/subnets/public/us-east-1c/linuxtips-public-1c"
+]
+
+ssm_private_subnets = [
+  "/linuxtips-vpc/subnets/private/us-east-1a/linuxtips-private-1a",
+  "/linuxtips-vpc/subnets/private/us-east-1b/linuxtips-private-1b",
+  "/linuxtips-vpc/subnets/private/us-east-1c/linuxtips-private-1c"
+]
+
+ssm_pod_subnets = [
+  "/linuxtips-vpc/subnets/private/us-east-1a/linuxtips-pods-1a",
+  "/linuxtips-vpc/subnets/private/us-east-1b/linuxtips-pods-1b",
+  "/linuxtips-vpc/subnets/private/us-east-1c/linuxtips-pods-1c"
+]
+
---
+++ variables.tf
@@ -0,0 +1,29 @@
+variable "project_name" {
+  type        = string
+  description = "Nome do projeto / cluster"
+}
+
+variable "region" {
+  type        = string
+  description = "Nome da região onde os recursos serão entregues"
+}
+
+variable "ssm_vpc" {
+  type        = string
+  description = "ID do SSM onde está o id da VPC onde o projeto será criado"
+}
+
+variable "ssm_public_subnets" {
+  type        = list(string)
+  description = "Lista dos ID's do SSM onde estão as subnets públicas do projeto"
+}
+
+variable "ssm_private_subnets" {
+  type        = list(string)
+  description = "Lista dos ID's do SSM onde estão as subnets privadas do projeto"
+}
+
+variable "ssm_pod_subnets" {
+  type        = list(string)
+  description = "Lista dos ID's do SSM onde estão as subnets de pods do projeto"
+}
