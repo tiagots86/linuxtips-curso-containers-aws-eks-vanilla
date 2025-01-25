7e7a80a236aae61a34eabb1bbf47d8afbc3fba8e
Tiago Teixeira Santos <tiagots86@hotmail.com>
2025-01-25 13:02:13 -0300

    profile fargate wildcard

--- fargate.tf
+++ fargate.tf
@@ -1,12 +1,25 @@
-resource "aws_eks_fargate_profile" "chip" {
+# resource "aws_eks_fargate_profile" "chip" {
+#   cluster_name         = aws_eks_cluster.main.name
+#   fargate_profile_name = "chip"
+
+#   pod_execution_role_arn = aws_iam_role.fargate.arn
+
+#   subnet_ids = data.aws_ssm_parameter.pod_subnets[*].value
+
+#   selector {
+#     namespace = "chip"
+#   }
+# }
+
+resource "aws_eks_fargate_profile" "wildcard" {
   cluster_name         = aws_eks_cluster.main.name
-  fargate_profile_name = "chip"
+  fargate_profile_name = "wildcard"
 
   pod_execution_role_arn = aws_iam_role.fargate.arn
 
   subnet_ids = data.aws_ssm_parameter.pod_subnets[*].value
 
   selector {
-    namespace = "chip"
+    namespace = "*"
   }
 }
