0b787bdfcb6e8f640256f43478f874ecd535a2a1
Tiago Teixeira Santos <tiagots86@hotmail.com>
2025-01-25 12:26:35 -0300

    fargate profile

--- acces_entries.tf
+++ acces_entries.tf
@@ -3,3 +3,9 @@ resource "aws_eks_access_entry" "nodes" {
   principal_arn = aws_iam_role.eks_nodes_role.arn
   type          = "EC2_LINUX"
 }
+
+resource "aws_eks_access_entry" "fargate" {
+  cluster_name  = aws_eks_cluster.main.id
+  principal_arn = aws_iam_role.fargate.arn
+  type          = "FARGATE_LINUX"
+}
