410d4c510ccfc3859fcca7c36089e0480d58770b
Tiago Teixeira Santos <tiagots86@hotmail.com>
2025-02-03 18:54:56 -0300

    role do karpenter + helm

---
+++ helm_karpenter.tf
@@ -0,0 +1,35 @@
+resource "helm_release" "karpenter" {
+  namespace        = "karpenter"
+  create_namespace = true
+
+  name       = "karpenter"
+  repository = "oci://public.ecr.aws/karpenter"
+  chart      = "karpenter"
+  version    = "1.0.8"
+
+  set {
+    name  = "serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
+    value = aws_iam_role.karpenter.arn
+  }
+
+  set {
+    name  = "settings.clusterName"
+    value = var.project_name
+  }
+
+  set {
+    name  = "settings.clusterEndpoint"
+    value = aws_eks_cluster.main.endpoint
+  }
+
+  set {
+    name  = "aws.defaultInstanceProfile"
+    value = aws_iam_instance_profile.nodes.name
+  }
+
+  depends_on = [
+    aws_eks_cluster.main,
+    aws_eks_node_group.main
+  ]
+
+}
---
+++ iam_karpenter.tf
@@ -0,0 +1,69 @@
+data "aws_iam_policy_document" "karpenter" {
+  statement {
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+    effect  = "Allow"
+
+    principals {
+      identifiers = [aws_iam_openid_connect_provider.eks.arn]
+      type        = "Federated"
+    }
+  }
+}
+
+resource "aws_iam_role" "karpenter" {
+  assume_role_policy = data.aws_iam_policy_document.karpenter.json
+  name               = format("%s-karpenter", var.project_name)
+}
+
+
+data "aws_iam_policy_document" "karpenter_policy" {
+  version = "2012-10-17"
+
+  statement {
+
+    effect = "Allow"
+    actions = [
+      "eks:DescribeCluster",
+      "ec2:CreateLaunchTemplate",
+      "ec2:CreateFleet",
+      "ec2:CreateTags",
+      "ec2:DescribeLaunchTemplates",
+      "ec2:DescribeInstances",
+      "ec2:DescribeSecurityGroups",
+      "ec2:DescribeSubnets",
+      "ec2:DescribeImages",
+      "ec2:DescribeInstanceTypes",
+      "ec2:DescribeInstanceTypeOfferings",
+      "ec2:DescribeAvailabilityZones",
+      "ec2:DescribeSpotPriceHistory",
+      "pricing:GetProducts",
+      "ec2:RunInstances",
+      "ec2:TerminateInstances",
+      "ec2:DeleteLaunchTemplate",
+      "ssm:GetParameter",
+      "iam:PassRole",
+      "sqs:*"
+    ]
+
+    resources = [
+      "*"
+    ]
+
+  }
+}
+
+resource "aws_iam_policy" "karpenter" {
+  name   = format("%s-karpenter", var.project_name)
+  path   = "/"
+  policy = data.aws_iam_policy_document.karpenter_policy.json
+}
+
+
+resource "aws_iam_policy_attachment" "karpenter" {
+  name = "karpenter"
+  roles = [
+    aws_iam_role.karpenter.name
+  ]
+
+  policy_arn = aws_iam_policy.karpenter.arn
+}
