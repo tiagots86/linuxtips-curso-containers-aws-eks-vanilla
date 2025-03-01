4c7934149f7927c9c133f452450415c657df34bd
Tiago Teixeira Santos <tiagots86@hotmail.com>
2025-03-01 10:20:50 -0300

    ajustes ebs storage class

--- ebs_storage_class.tf
+++ ebs_storage_class.tf
@@ -14,7 +14,9 @@ allowVolumeExpansion: true
 YAML
 
   depends_on = [
-    aws_eks_addon.ebs_csi,
+    aws_eks_cluster.main,
+    aws_eks_addon.ebs,
     aws_eks_pod_identity_association.ebs_csi
   ]
+
 }
