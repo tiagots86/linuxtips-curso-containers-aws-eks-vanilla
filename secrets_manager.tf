697e647fd8cbe341da8beec763ca8b81e6962863
Tiago Teixeira Santos <tiagots86@hotmail.com>
2025-03-08 17:50:14 -0300

    secrets manager json

--- secrets_manager.tf
+++ secrets_manager.tf
@@ -6,3 +6,16 @@ resource "aws_secretsmanager_secret_version" "teste" {
   secret_id     = aws_secretsmanager_secret.teste.id
   secret_string = "BAR"
 }
+
+resource "aws_secretsmanager_secret" "teste_json" {
+  name = "chip-teste-json"
+}
+
+resource "aws_secretsmanager_secret_version" "teste_json" {
+  secret_id = aws_secretsmanager_secret.teste_json.id
+  secret_string = jsonencode({
+    foo      = "BAR",
+    username = "admin",
+    password = "abc123"
+  })
+}
