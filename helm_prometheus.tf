efa5842c426a8f69b67e78508621b7474366ad58
Tiago Teixeira Santos <tiagots86@hotmail.com>
2025-03-29 09:09:20 -0300

    limpeza para aula

--- helm_prometheus.tf
+++ helm_prometheus.tf
@@ -18,31 +18,31 @@ resource "helm_release" "prometheus" {
   ]
 }
 
-resource "kubectl_manifest" "grafana_host" {
-  yaml_body = <<YAML
-apiVersion: networking.k8s.io/v1
-kind: Ingress
-metadata:
-  name: grafana-ingress
-  namespace: prometheus
-  annotations:
-spec:
-  ingressClassName: nginx
-  rules:
-    - host: ${var.grafana_host}
-      http:
-        paths:
-          - path: /
-            pathType: Prefix
-            backend:
-              service:
-                name: prometheus-grafana
-                port:
-                  number: 80
-YAML
-  depends_on = [
-    helm_release.prometheus,
-    helm_release.nginx_controller
-  ]
+# resource "kubectl_manifest" "grafana_host" {
+#   yaml_body = <<YAML
+# apiVersion: networking.k8s.io/v1
+# kind: Ingress
+# metadata:
+#   name: grafana-ingress
+#   namespace: prometheus
+#   annotations:
+# spec:
+#   ingressClassName: nginx
+#   rules:
+#     - host: ${var.grafana_host}
+#       http:
+#         paths:
+#           - path: /
+#             pathType: Prefix
+#             backend:
+#               service:
+#                 name: prometheus-grafana
+#                 port:
+#                   number: 80
+# YAML
+#   depends_on = [
+#     helm_release.prometheus,
+#     helm_release.nginx_controller
+#   ]
 
-}
+# }
