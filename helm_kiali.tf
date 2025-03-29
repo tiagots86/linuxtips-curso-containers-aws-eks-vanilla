05433a453c20da9367355651c4d25712c1191de2
Tiago Teixeira Santos <tiagots86@hotmail.com>
2025-03-29 11:27:01 -0300

    customizações kiali

--- helm_kiali.tf
+++ helm_kiali.tf
@@ -18,6 +18,109 @@ resource "helm_release" "kiali-server" {
     value = "anonymous"
   }
 
+  set {
+    name  = "external_services.tracing.use_grpc"
+    value = "false"
+  }
+
+  set {
+    name  = "external_services.tracing.enabled"
+    value = "true"
+  }
+
+  set {
+    name  = "external_services.tracing.internal_url"
+    value = "http://jaeger-query.tracing.svc.cluster.local:16686"
+  }
+
+    set {
+    name  = "external_services.tracing.external_url"
+    value = format("http://%s", var.jaeger_host)
+  }
+
+
+  set {
+    name  = "external_services.prometheus.url"
+    value = "http://prometheus-kube-prometheus-prometheus.prometheus.svc.cluster.local:9090"
+  }
+
+
+  set {
+    name  = "external_services.grafana.enabled"
+    value = "true"
+  }
+
+  set {
+    name  = "external_services.grafana.external_url"
+    value = format("http://%s", var.grafana_host)
+  }
+
+  set {
+    name  = "external_services.grafana.internal_url"
+    value = "http://prometheus-grafana.prometheus.svc.cluster.local:80"
+  }
+
+  set {
+    name  = "external_services.grafana.auth.type"
+    value = "basic"
+  }
+
+  set {
+    name  = "external_services.grafana.auth.insecure_skip_verify"
+    value = "true"
+  }
+
+  set {
+    name  = "external_services.grafana.auth.username"
+    value = "admin"
+  }
+
+  set {
+    name  = "external_services.grafana.auth.password"
+    value = "linuxtips"
+  }
+
+  set {
+    name  = "external_services.grafana.dashboards[0].name"
+    value = "Istio Mesh Dashboard"
+  }
+
+  set {
+    name  = "external_services.grafana.dashboards[1].name"
+    value = "Istio Service Dashboard"
+  }
+  set {
+    name  = "external_services.grafana.dashboards[1].variables.namespace"
+    value = "var-namespace"
+  }
+
+  set {
+    name  = "external_services.grafana.dashboards[1].variables.service"
+    value = "var-service"
+  }
+
+  set {
+    name  = "external_services.grafana.dashboards[2].name"
+    value = "Istio Workload Dashboard"
+  }
+
+  set {
+    name  = "external_services.grafana.dashboards[2].variables.namespace"
+    value = "var-namespace"
+  }
+  
+  set {
+    name  = "external_services.grafana.dashboards[2].variables.workload"
+    value = "var-workload"
+  }
+
+  set {
+    name  = "external_services.grafana.dashboards[3].name"
+    value = "Istio Performance Dashboard"
+  }
+
+
+
   depends_on = [
     helm_release.istio_base,
     helm_release.istiod,
