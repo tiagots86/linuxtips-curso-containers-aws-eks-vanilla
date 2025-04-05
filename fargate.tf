e5fbcf451a06b435b0ee9d049dfd313a35ecf76d
Tiago Teixeira Santos <tiagots86@hotmail.com>
2025-04-05 10:59:33 -0300

    keda no fargate

--- fargate.tf
+++ fargate.tf
@@ -23,3 +23,16 @@ resource "aws_eks_fargate_profile" "karpenter" {
     namespace = "karpenter"
   }
 }
+
+resource "aws_eks_fargate_profile" "keda" {
+  cluster_name         = aws_eks_cluster.main.name
+  fargate_profile_name = "keda"
+
+  pod_execution_role_arn = aws_iam_role.fargate.arn
+
+  subnet_ids = data.aws_ssm_parameter.pod_subnets[*].value
+
+  selector {
+    namespace = "keda"
+  }
+}
