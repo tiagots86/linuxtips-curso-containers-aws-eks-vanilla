8a367edc0c0afe0f68450268092bf6e0e580c598
Tiago Teixeira Santos <tiagots86@hotmail.com>
2025-04-12 16:30:44 -0300

    argo rollouts no fargate

--- fargate.tf
+++ fargate.tf
@@ -36,3 +36,16 @@ resource "aws_eks_fargate_profile" "keda" {
     namespace = "keda"
   }
 }
+
+resource "aws_eks_fargate_profile" "rollouts" {
+  cluster_name         = aws_eks_cluster.main.name
+  fargate_profile_name = "argo-rollouts"
+
+  pod_execution_role_arn = aws_iam_role.fargate.arn
+
+  subnet_ids = data.aws_ssm_parameter.pod_subnets[*].value
+
+  selector {
+    namespace = "argo-rollouts"
+  }
+}
