aa107aa7105840b06956c076141fe6e73ea15cd0
Tiago Teixeira Santos <tiagots86@hotmail.com>
2025-04-26 17:28:47 -0300

    extensões do argocd

--- helm_argocd.tf
+++ helm_argocd.tf
@@ -12,6 +12,36 @@ resource "helm_release" "argocd" {
     value = "--insecure"
   }
 
+  set {
+    name  = "server.extensions.enabled"
+    value = "true"
+  }
+
+  set {
+    name  = "server.enable.proxy.extension"
+    value = "true"
+  }
+
+  set {
+    name  = "server.extensions.image.repository"
+    value = "quay.io/argoprojlabs/argocd-extension-installer"
+  }
+
+  set {
+    name  = "server.extensions.extensionList[0].name"
+    value = "rollout-extension"
+  }
+
+  set {
+    name  = "server.extensions.extensionList[0].env[0].name"
+    value = "EXTENSION_URL"
+  }
+
+  set {
+    name  = "server.extensions.extensionList[0].env[0].value"
+    value = "https://github.com/argoproj-labs/rollout-extension/releases/download/v0.3.6/extension.tar"
+  }
+
   depends_on = [
     aws_eks_cluster.main,
     aws_eks_fargate_profile.karpenter
